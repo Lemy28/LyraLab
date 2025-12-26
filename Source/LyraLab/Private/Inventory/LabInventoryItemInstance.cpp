d1b0700a298b99af164a18e71d5285ff5746a443
Lemy28 <1726736665@qq.com>
2025-12-26 15:45:50 +0800

    add item instance used in inventory list

---
+++ Source/LyraLab/Private/Inventory/LabInventoryItemInstance.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright Lemy. All Rights Reserved.
+
+
+#include "Inventory/LabInventoryItemInstance.h"
---
+++ Source/LyraLab/Public/Inventory/LabInventoryItemInstance.h
@@ -0,0 +1,16 @@
+﻿// Copyright Lemy. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "LabInventoryItemInstance.generated.h"
+
+/**
+ * Instance class used by inventory component
+ */
+UCLASS()
+class LYRALAB_API ULabInventoryItemInstance : public UObject
+{
+	GENERATED_BODY()
+};
